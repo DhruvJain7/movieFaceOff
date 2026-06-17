33e7d89c2d6f89809905d4a5c7603539ed29b556
Dhruv Jain7 <djmjtj10@gmail.com>
2026-06-17 22:32:16 +0530

    Handled the broken images

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("Hello there!");
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "ed57098e",
+      s: searchTerm,
+    },
+  });
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
+};
+
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+   <label><b>Search For a Movie</b></label>
+   <input class='input' />
+   <div class='dropdown'>
+   <div class = 'dropdown-menu'>
+   <div class ='dropdown-content results'></div>
+   </div>
+   </div>
+  `;
+// debouncing an event
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultWrapper = document.querySelector(".results");
+
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+
+  resultWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
+  console.log(movies);
+  for (let movie of movies) {
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+      <img src="${imgSrc}"/>
+      ${movie.Title}
+      `;
+    resultWrapper.appendChild(option);
+  }
+};
+input.addEventListener("input", debounce(onInput, 500));
